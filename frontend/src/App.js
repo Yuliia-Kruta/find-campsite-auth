210727e9a966e6ef05cb514aa09624d07b3d9a78
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2024-09-18 15:21:08 +1000

    Login component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {Router, Route, Routes} from 'react-router-dom'
+import Login from './components/Login'
+import Register from './components/Register'
+import ResetPassword from './components/ResetPassword'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import axios from 'axios';
+
+const Login = () => {
+    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setError('');
+        setMessage('');
+
+        try {
+            const response = await axios.post('http://127.0.0.1:5000/login', { email, password });
+            setMessage(response.data.message);
+        } catch (err) {
+            setError(err.response?.data?.error || 'Login failed');
+        }
+    };
+
+
+    return ( 
+        <div className="wrapper">
+            <form onSubmit={handleLogin}>
+                <h1>Login</h1>
+                <div className="input-box">
+                    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                </div>
+                <div className="input-box">
+                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                </div>
+                <div className="remember-forgot">
+                    <label htmlFor=""><input type="checkbox" />Remember me</label>
+                    <Link to="/reset-password">Forgot password?</Link>
+                </div>
+                <button type="submit">Login</button>
+                <div className="register-link">
+                    <p>Don't have an account? <Link to="/register">Register</Link></p>
+                </div>
+            </form>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            {message && <p style={{ color: 'green' }}>{message}</p>}
+        </div>
+     );
+}
+ 
+export default Login;
