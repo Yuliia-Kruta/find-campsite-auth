6d80d2a31a3a99a0086f1a83ffa0d04693f59b5b
Yuliia Kruta <130898660+Yuliia-Kruta@users.noreply.github.com>
2025-02-21 21:18:24 +1000

    Backend base url fixed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || "https://find-campsite-auth-backend.onrender.com";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:5000";
 export default API_BASE_URL;
